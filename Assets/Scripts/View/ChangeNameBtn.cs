a8d08d200b3faf67d17664daeb9d4503abe94f46
FavorKim <harangs99@daum.net>
2024-06-14 15:23:34 +0900

    [feat] 이름 랜덤 변경 기능 추가

--- Assets/Scripts/View/ChangeNameBtn.cs
+++ Assets/Scripts/View/ChangeNameBtn.cs
@@ -1,9 +1,16 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class ChangeNameBtn : MonoBehaviour
 {
+    List<string> names = new List<string>
+    {
+        "기러기", "부엉이", "따오기", "소쩍새", "직박구리", "오목눈이", "비둘기", "왜가리"
+    };
     public void OnClick_RequestChangeName()
     {
-        GameLogicManager.Inst.RequestChangeName("변경");
+        int index = 0;
+        index = Random.Range(0, names.Count);
+        GameLogicManager.Inst.RequestChangeName(names[index]);
     }
 }
